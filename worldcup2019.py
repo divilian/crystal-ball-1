dfe92b1c489f6694447950a85489ab46fce5e50a
Stephen <stephen@umw.edu>
2019-10-19 21:11:24 -0400

    Finish Tables in Python chapter (2 of 3).

---
+++ worldcup2019.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats
+import io
+
+wc = pd.read_csv(io.StringIO('''
+last,first,date,in_mins,in_secs,out_mins,out_secs,goals,assists,tackles,fouls
+Morgan,Alex,28-Jun,0,0,90,0,0,0,2,1
+Rapinoe,Megan,28-Jun,0,0,74,27,2,0,2,2
+Press,Christen,28-Jun,74,27,90,0,0,0,1,0
+Lavelle,Rose,28-Jun,0,0,90,0,0,1,3,0
+Lavelle,Rose,7-Jul,0,0,90,0,1,0,4,1
+Rapinoe,Megan,7-Jul,0,0,83,16,1,1,3,2
+Lloyd,Carli,7-Jul,83,16,90,0,0,0,1,0
+Dunn,Crystal,23-Jun,42,37,81,5,0,1,1,2
+'''))
+
+wc['in_time'] = np.round(wc['in_mins'] + wc['in_secs'] / 60,2)
+wc['out_time'] = np.round(wc['out_mins'] + wc['out_secs'] / 60,2)
+del wc['in_mins']
+del wc['in_secs']
+del wc['out_mins']
+del wc['out_secs']
+
+wc['mins_played'] = wc['out_time'] - wc['in_time']
+
+wc['starter'] = np.where(wc['in_time'] == 0, True, False)
+del wc['in_time']
+del wc['out_time']
+print(wc)
