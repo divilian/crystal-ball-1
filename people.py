9e4be63beec727a2ce6cc2ac22eb17a621cf9859
Stephen <stephen@umw.edu>
2019-11-03 16:57:37 -0500

    Finish t-test section.

--- people.py
+++ people.py
@@ -8,3 +8,19 @@ people = pd.read_csv('people.csv')
 gender_color = pd.crosstab(people.gender, people.color)
 print(gender_color)
 print(scipy.stats.chi2_contingency(gender_color))
+
+print(scipy.stats.chi2_contingency(gender_color))
+
+people.boxplot('salary',by='gender')
+
+print(people.groupby('gender')['salary'].mean())
+
+print(people.groupby('gender')['followers'].mean())
+
+print(scipy.stats.ttest_ind(
+    people[people.gender=="female"]['followers'],
+    people[people.gender=="male"]['followers']))
+
+print(scipy.stats.ttest_ind(
+    people[people.gender=="female"]['salary'],
+    people[people.gender=="male"]['salary']))
