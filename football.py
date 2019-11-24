3aec4102db5732cb5c3b3c305d618ae6b7a382dc
Stephen <stephen@umw.edu>
2019-11-24 14:28:18 -0500

    Finish training-vs-test-data section.

--- football.py
+++ football.py
@@ -11,7 +11,15 @@ age,hometown,current_residence,years_in_residence,team
 47,Allegheny,Chevy Chase,13,Redskins
 31,Littleton,Littleton,3,Broncos
 51,Orange County,Dumfries,23,Redskins
-32,Warrenton,Orlando,1,Redskins
+32,Warrenton,Winchester,11,Redskins
+39,Dumfries,Miami,5,Redskins
+31,Warrenton,Charlottesville,6,Jets
+52,Arlington,Fredericksburg,17,Redskins
+60,Tyson's Corner,Falls Church,4,Cowboys
+17,Fredericksburg,Fredericksburg,17,Redskins
+29,Fredericksburg,Seattle,21,Seahawks
+37,Richmond,Richmond,37,Redskins
+35,Mechanicsville,Orlando,8,Redskins
 19,New York,New York,19,Giants
 63,New York,Orlando,17,Giants
 42,Alexandria,Manassas,10,Redskins
@@ -22,3 +30,7 @@ age,hometown,current_residence,years_in_residence,team
 
 print(fans)
 
+training = fans.sample(frac=.7)
+print(training)
+test = fans[~fans.index.isin(training.index)]
+print(test)
