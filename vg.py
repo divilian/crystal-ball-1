49334f5c8ce4478c68b4a18bec06f45a6283a609
Stephen <stephen@umw.edu>
2019-12-04 21:20:10 -0500

    Finish Evaluating a Classifier chapter.

--- vg.py
+++ vg.py
@@ -49,3 +49,58 @@ PSYC,middle,F,Yes
 ))
 
 print(students)
+
+students_test = pd.read_csv(io.StringIO('''
+Major,Age,Gender,VG
+CPSC,young,F,No
+CPSC,old,O,No
+MATH,old,F,No
+CPSC,middle,M,No
+PSYC,middle,F,Yes
+PSYC,young,F,No
+MATH,old,M,Yes
+CPSC,middle,M,Yes
+'''
+))
+
+print(students_test)
+
+def predict(major, age, gender):
+    if gender == 'M':
+        if age == 'young':
+            return 'Yes'
+        elif age == 'middle':
+            if major == 'PSYC' or major == 'CPSC':
+                return 'Yes'
+            elif major == 'MATH':
+                return 'No'
+        elif age == 'old':
+            return 'Yes'
+    elif gender == 'F':
+        if major == 'MATH':
+            return 'No'
+        elif major == 'CPSC':
+            if age == 'young' or age == 'middle':
+                return 'No'
+            elif age == 'old':
+                return 'Yes'
+        elif major == 'PSYC':
+            if age == 'young' or age == 'old':
+                return 'No'
+            elif age == 'middle':
+                return 'Yes'  # Here's our "contradiction"
+    elif gender == 'O':
+        return 'No'
+
+count = 0
+for row in students_test.itertuples():
+    if predict(row.Major, row.Age, row.Gender) == row.VG:
+        print("  Predicted {}/{}/{} right!".format(row.Major,row.Age,row.Gender))
+        count += 1
+    else:
+        print("X Predicted {}/{}/{} wrong. :(".format(row.Major,row.Age,row.Gender))
+
+accuracy = count / len(students_test) * 100
+print("Our accuracy on the test set was {}% ({}/{}).".format(accuracy,
+    count, len(students_test)))
+
