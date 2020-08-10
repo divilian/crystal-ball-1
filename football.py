4629e37e7eaee369fbffd2bec2d9d6eba1b7a909
Stephen <stephen@umw.edu>
2020-08-10 13:05:53 -0400

    Purge offensive Washington NFL name.

--- football.py
+++ football.py
@@ -6,26 +6,26 @@ import io
 
 fans = pd.read_csv(io.StringIO('''
 age,hometown,current_residence,years_in_residence,team
-29,Warrenton,Warrenton,29,Redskins
+29,Warrenton,Warrenton,29,Ravens
 39,Lubbock,Arlington,2,Cowboys
-47,Allegheny,Chevy Chase,13,Redskins
+47,Allegheny,Chevy Chase,13,Ravens
 31,Littleton,Littleton,3,Broncos
-51,Orange County,Dumfries,23,Redskins
-32,Warrenton,Winchester,11,Redskins
-39,Dumfries,Miami,5,Redskins
+51,Orange County,Dumfries,23,Ravens
+32,Warrenton,Winchester,11,Ravens
+39,Dumfries,Miami,5,Ravens
 31,Warrenton,Charlottesville,6,Jets
-52,Arlington,Fredericksburg,17,Redskins
+52,Arlington,Fredericksburg,17,Ravens
 60,Tyson's Corner,Falls Church,4,Cowboys
-17,Fredericksburg,Fredericksburg,17,Redskins
+17,Fredericksburg,Fredericksburg,17,Ravens
 29,Fredericksburg,Seattle,21,Seahawks
-37,Richmond,Richmond,37,Redskins
-35,Mechanicsville,Orlando,8,Redskins
+37,Richmond,Richmond,37,Ravens
+35,Mechanicsville,Orlando,8,Ravens
 19,New York,New York,19,Giants
 63,New York,Orlando,17,Giants
-42,Alexandria,Manassas,10,Redskins
+42,Alexandria,Manassas,10,Ravens
 22,Allegheny,Harrisburg,1,Steelers
-38,Mechanicsville,Fredericksburg,6,Redskins
-32,Warrenton,Orlando,1,Redskins
+38,Mechanicsville,Fredericksburg,6,Ravens
+32,Warrenton,Orlando,1,Ravens
 '''))
 
 print(fans)
